c0b13abae3767a4951a7c3cff313c04dda4b695c
Michael Xiang <50655871+UserUnknownX@users.noreply.github.com>
2020-04-25 07:33:15 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?110238a0202398bded1e30a837879f8b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
